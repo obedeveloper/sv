c0757607ef35683b2b85b7c283277bca09c4426c
NIYOMUGISHA Obed <obed6@duck.com>
2026-01-07 04:59:21 +0200

    Add support for turso in production

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+const prod = process.env.ENVIRONMENT === 'PROD';
+
+if (!prod && !process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (prod && (!process.env.TURSO_CONNECTION_URL || !process.env.TURSO_AUTH_TOKEN)) {
+	throw new Error('Turso URL and TOKEN are required');
+}
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema/index.ts',
 	out: './src/lib/server/db/migrations/',
-	dialect: 'sqlite',
-	dbCredentials: { url: process.env.DATABASE_URL },
+	dialect: prod ? 'turso' : 'sqlite',
+	dbCredentials: {
+		url: prod ? process.env.TURSO_CONNECTION_URL! : process.env.DATABASE_URL!,
+		authToken: prod ? process.env.TURSO_AUTH_TOKEN : undefined
+	},
 	verbose: true,
 	strict: true
 });
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,9 +2,19 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { dev } from '$app/environment';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+dev ? validateSQLITE() : validateTURSO();
 
 const client = createClient({ url: env.DATABASE_URL });
 
 export const db = drizzle(client, { schema });
+
+function validateSQLITE() {
+	if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+}
+
+function validateTURSO() {
+	if (!env.TURSO_CONNECTION_URL || !env.TURSO_AUTH_TOKEN)
+		throw new Error('Turso URL and TOKEN are required');
+}
